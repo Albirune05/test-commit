362c6d8c82b1dcbc4e6cdfb0f2d4f6b8126f53b3
Albirune <mohaalbirune@gmail.com>
2026-08-08 19:06:45 +0300

    automation commit

--- src/main/java/com/visium/democommit/controllers/controleur.java
+++ src/main/java/com/visium/democommit/controllers/controleur.java
@@ -1,7 +1,11 @@
 package com.visium.democommit.controllers;
 
+import com.visium.democommit.model.personne;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api")
@@ -9,5 +13,13 @@ public class controleur {
     @RequestMapping("/hello")
     public String hello() {
         return "Hello World!";
+    }
+
+@GetMapping("/personnes")
+    public personne personne() {
+        return new personne(UUID.randomUUID().toString(),"Ali","ali@gmail.com");
+
+
+
     }
 }
--- src/main/java/com/visium/democommit/model/personne.java
+++ src/main/java/com/visium/democommit/model/personne.java
@@ -1,4 +1,4 @@
 package com.visium.democommit.model;
 
-public record personne() {
+public record personne(String id, String name, String email) {
 }
